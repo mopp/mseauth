58c2c93e96590be71cf1bb8bcebd32d7cce4ada7
mopp <hello@mopp.jp>
2021-05-15 00:40:16 +0900

    init

---
+++ lib/mseauth.ex
@@ -0,0 +1,18 @@
+defmodule Mseauth do
+  @moduledoc """
+  Documentation for `Mseauth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mseauth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mseauth_test.exs
@@ -0,0 +1,8 @@
+defmodule MseauthTest do
+  use ExUnit.Case
+  doctest Mseauth
+
+  test "greets the world" do
+    assert Mseauth.hello() == :world
+  end
+end
